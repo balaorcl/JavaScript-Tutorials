82f4a718bb05c5c3a6e973590f3c2c9f11861d41
Bala <47841287+balaorcl@users.noreply.github.com>
2021-08-14 22:39:38 +0400

    Add files via upload

---
+++ 3-arithmetic/script.js
@@ -0,0 +1,23 @@
+var friends = 10;
+
+// friends = friends + 1;
+// friends = friends -1;
+// friends = friends * 2;
+// friends = friends / 2;
+// friends = friends ** 2;
+var remaniningFriends = friends % 3;
+
+console.log(friends)
+console.log(remaniningFriends)
+
+/*
+PEMDAS
+Operator precedence
+1. parantheses
+2. exponents
+3. multiply * & divide /
+4. addition + & subtraction - 
+*/
+
+friends = 10 + 2 / 2;
+console.log(friends)
