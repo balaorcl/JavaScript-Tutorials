b01633b8232a84d2bec2cd6297144cbd09a5bd0d
Bala <47841287+balaorcl@users.noreply.github.com>
2021-08-14 22:07:57 +0400

    Add files via upload

---
+++ 2-variables/script.js
@@ -0,0 +1,30 @@
+var age = 21;
+var firstName = "Bala";
+var lastName = "Code";
+var myCoolVariable;
+var online = true;
+
+myCoolVariable = "Code";
+
+console.log("Your age is "+age)
+console.log("Your age is ",age)
+console.log("Hello",firstName);
+console.log("Are you online?:", online);
+console.log(myCoolVariable);
+
+age = age + 1;
+console.log("Your age is ",age)
+
+var fullName = firstName + lastName;
+console.log("Hello", fullName);
+
+// limited scope
+let mySweetVarible = 420;
+console.log("mySweetVarible", mySweetVarible);
+
+// constant
+const pi = 3.14159;
+
+pi = 4;
+
+console.log(pi);
